aea56565458f30e19c90d0ca2c304880fbf67d26
Tiago Dias <98658691+tdiascontato@users.noreply.github.com>
2023-06-15 08:54:41 -0300

    Delete routes.js

--- routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const route = express.Router();
-const HomeController = require('./src/Controllers/HomeController');
-const ContatoController = require('./src/Controllers/ContatoController');
-route.get('/', HomeController.PaginaInicial);
-route.post('/', HomeController.Post);
-route.get('/contato', ContatoController.PaginaInicial);
-module.exports = route;
