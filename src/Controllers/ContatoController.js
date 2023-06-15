d31704e2874461376f7eff3da86073c59d4bfc3b
Tiago Dias <98658691+tdiascontato@users.noreply.github.com>
2023-06-15 08:53:43 -0300

    Delete src directory

--- src/Controllers/ContatoController.js
+++
@@ -1,3 +0,0 @@
-exports.PaginaInicial = (req,res) =>{
-    res.send('Olá! Você já viu meu github? Acesse github.com/tdiascontato !')
-};
--- src/Controllers/HomeController.js
+++
@@ -1,6 +0,0 @@
-exports.PaginaInicial = (req, res) =>{
-    res.render('index');
-};
-exports.Post = (req, res) =>{
-    res.send(`Opa, meu amigo! Recebido!`);
-};
